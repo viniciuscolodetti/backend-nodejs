07348347bbf10f94e28481de651454fac2ea76bd
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2020-08-23 21:16:12 -0300

    Initial commit

---
+++ src/shared/container/index.ts
@@ -0,0 +1,21 @@
+import { container } from 'tsyringe';
+
+import './providers';
+
+import '@modules/users/providers/index';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
+import IUserTokenRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
+
+container.registerSingleton<IUserTokenRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
+);
---
+++ src/shared/infra/http/routes/index.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+
+const routes = Router();
+
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
+routes.use('/profile', profileRouter);
+routes.use('/password', passwordRouter);
+
+export default routes;
